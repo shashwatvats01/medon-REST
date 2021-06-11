bf2bc62a1a09bfe6ae6543215b285f6607960fa5
Shashwat Vats <shashwat.singh.vats@gmail.com>
2021-06-11 13:45:57 +0530

    New Project Added

---
+++ medon-REST-API-DEV/src/main/java/com/medonrestapi/dev/MedonRestApiDevApplication.java
@@ -0,0 +1,13 @@
+package com.medonrestapi.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedonRestApiDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedonRestApiDevApplication.class, args);
+	}
+
+}
